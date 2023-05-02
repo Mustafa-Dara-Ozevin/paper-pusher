516eeade19f41d797ed2d019bf04173da088b544
Mustafa-Dara-Ozevin <mustafadara.ozevin@outlook.com>
2023-05-02 22:57:07 +0200

    fix templates

--- src-tauri/src/templates.rs
+++ src-tauri/src/templates.rs
@@ -1,11 +1,11 @@
 pub const BOOST: &str = 
 "OFFICER responded to dispatch call about CAR being stolen at LOCATION. Upon arriving at scene OFFICER saw the CAR and attempted to pull it over. SUSPECT disregarded 
 orders to pull over and started recklessly evading by going over 100mph and driving on opposite lanes. Suspect was eventually apprehended at LOCATION after resisting 
-arrest by breaking out of cuffs. Upon further inspection there were signs of tampering in the driver side of the CAR. Car was documented and impounded. Suspect was 
+arrest by breaking out of cuffs. Upon further inspection there were signs of tampering in the driver side of the CAR. Car was documented and impounded. SUSPECT was 
 charged with Grand Theft Auto, Reckless evading and Resisting arrest which they plead guilty/not guilty to.";
 
 pub const CONTROL_DEVICES: &str = 
-"OFFICER saw CAR going through red light(s) and attempted to pull it over. SUSPECT complied and identified themselves. They were give a ticket for Failure to obey traffic control devices.";
+"OFFICER saw CAR going through red light(s) and attempted to pull it over. SUSPECT complied and identified themselves. SUSPECT was given a ticket for Failure to obey traffic control devices.";
 
 pub const NEGLIGENT_DRIVING: &str = 
 "OFFICER saw CAR going through red light(s) and going OVERSPEEDLIMIT at a SPEEDLIMIT. Officer attempted to pull the CAR over, SUSPECT complied and identified themselves. They were give a ticket for Negligent Driving.";
@@ -18,7 +18,7 @@ pub const GRS: &str =
 pub const HOUSE_ROBBERY: &str = 
 "OFFICER responded to a house alarm at LOCATION. Upon arriving at the area OFFICER saw SUSPECT and attempted to detain him for questioning. SUSPECT tried to flee on foot/with CAR 
 and was eventually apprehended after resisting arrest by trying to break cuffs. SUSPECT had scratches on their hands and their DNA matched with the blood on the door of the house
- that was being robbed they were also in Possession of stolen goods from the house. Suspect was charged with Robbery, Resisting Arrest, Possession of third degree stolen goods 
+ that was being robbed they were also in Possession of stolen goods from the house. SUSPECT was charged with Robbery, Resisting Arrest, Possession of third degree stolen goods 
  which they plead guilty/ not guilty to.";
 
 pub const METH_RUN: &str = 
